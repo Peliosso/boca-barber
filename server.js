7088c152281460c53027b301828341d86b71d6f5
JayV <66281743+Peliosso@users.noreply.github.com>
2024-06-06 10:36:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 // Rota para lidar com os agendamentos
 app.post('/agendamentos', (req, res) => {
     const { name, phone, service, date, time } = req.body;
-    // Aqui você pode processar os dados do agendamento como quiser
     console.log('Novo agendamento:');
     console.log('Nome:', name);
     console.log('Telefone:', phone);
     console.log('Serviço:', service);
     console.log('Data:', date);
     console.log('Hora:', time);
-
-    // Aqui você pode salvar os dados em um banco de dados, enviar e-mails, etc.
 
     res.json({ message: 'Agendamento recebido com sucesso!' });
 });
